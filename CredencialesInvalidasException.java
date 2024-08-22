9952261e1e77039b79b7715b1a6b9a060f919b5d
ManuelAnJimenez <maanjime@espol.edu.ec>
2024-08-22 13:46:26 -0500

    Add files via upload

---
+++ CredencialesInvalidasException.java
@@ -0,0 +1,10 @@
+package Modelo;
+
+public class CredencialesInvalidasException extends Exception{
+
+        public CredencialesInvalidasException(String message) {
+            super(message);
+        }
+
+
+    }
